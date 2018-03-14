17cd4223b2e7ef63870aad903baf06a52f926fab
jtliso <jtliso@resourcemanager.jtliso.educationproject-pg0.wisc.cloudlab.us>
2018-03-13 19:58:09 -0500

    added a setup script and added to run script

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 chmod 700 src/wordcount_mapper.py
 chmod 700 src/wordcount_reducer.py
+
+export HADOOP_HOME=/usr/local/hadoop-2.7.3
+export PATH=$PATH:/usr/local/hadoop-2.7.3/bin
+
+`hadoop jar /usr/local/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar -files src/wordcount_mapper.py,src/wordcount_reducer.py -mapper src/wordcount_mapper.py -reducer src/wordcount_reducer.py -input /test/shakespeare.txt -output /test/output.txt`
+
+
